811de135b74518d72a067cc87d65d2de78c6cc0f
Snigdha171 <prakash17snigdha@gmail.com>
2016-09-11 03:47:23 +0530

    Add files via upload

---
+++ Location.js
@@ -0,0 +1,6 @@
+Location = [
+[23.3440997,85.309562, 'Ranchi, Jharkhand, India'],
+[15.9128998,79.7399875, 'Andhra Pradesh, India'],
+[12.9715987,77.5945627, 'Bengaluru, Karnataka 560001, India'],
+
+];
